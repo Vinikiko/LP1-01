9858e2cf09607c27c565bc7212fc64e41b5cf607
Vinikiko <162053438+Vinikiko@users.noreply.github.com>
2026-03-10 17:26:37 -0300

    Update calcfatorial.c

--- calcfatorial/calcfatorial.c
+++ calcfatorial/calcfatorial.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
 
-int main void() 
+int main() 
 {
-  int n
+    int n;
+    int soma = 1;
+    printf("Digite um numero inteiro: ");
+    scanf("%d", &n);
+    for (int i = 1; i <=n; i++)
+    {
+        soma = soma * i;
+    }
+    printf("%d\n", soma);
+    return 0;   
 }
